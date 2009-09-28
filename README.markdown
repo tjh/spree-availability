6913d552ed9c21846efef3fd9905055601fee039
Tim Harvey <tim@literacy5.com>
2009-09-28 09:32:33 -0400

    Cleaned up Readme and Extension activation

--- README.markdown
+++ README.markdown
@@ -1,3 +1,14 @@
-= Availability
+= Availability Extension
 
-Description goes here
+Similar to Amazon's "This product ships within 24 hours" or "Should ship in 4-5 days", this extension provides two messages for each product that give customers more information on the item's availability.
+
+One message displays when the product (or an individual variant) has zero stock ("Out of stock, ships in 3-4 days", for example) and another when there is 1 or more in stock ("In stock, ships within 24 hours", for example).
+
+Because a product may have some variants in stock and others out of stock, you can setup the product pages to display one of the two messages chosen for the product based on those stock levels.
+
+The store can have numerous messages available for products to use.
+
+= To Do
+
+* Setup the message table to allow multiple languages
+* Consider ways to apply messages to entire blocks (categories, product types, etc) at once to speed administration
--- availabilities_extension.rb
+++ availabilities_extension.rb
@@ -1,32 +1,9 @@
-# Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application'
-
 class AvailabilitiesExtension < Spree::Extension
   version "1.0"
-  description "Describe your extension here"
-  url "http://yourwebsite.com/availability"
+  description "Choose custom messages to display when a product is in stock or out of stock"
+  url "http://timharvey.net"
 
-  # Please use availability/config/routes.rb instead for extension routes.
-
-  # def self.require_gems(config)
-  #   config.gem "gemname-goes-here", :version => '1.2.3'
-  # end
-  
   def activate
-
-    # Add your extension tab to the admin.
-    # Requires that you have defined an admin controller:
-    # app/controllers/admin/yourextension_controller
-    # and that you mapped your admin in config/routes
-
-    Admin::ConfigurationsController.class_eval do
-      before_filter :add_availability_link, :only => :index
-
-      def add_availability_link
-        @extension_links << {:link => admin_availabilities_path, :link_text => t('tab_availability'), :description => t('tab_availability_desc')}
-      end
-    end
-
     Variant.class_eval do
       def availability_message_out_of_stock
         Availability.find( self.product.availability_id_out_of_stock ).text
@@ -38,13 +15,11 @@ class AvailabilitiesExtension < Spree::Extension
     end
 
     Admin::BaseController.class_eval do
-      
       before_filter :add_product_availability_tab
 
       def add_product_availability_tab
         @product_admin_tabs << {:name => t("tab_product_availability"), :url => "admin_product_view_availability_url"}
       end
-
 
       # Removed the following and decided to leave it in the "Configuration" tab
       # instead of its own tab.
@@ -54,11 +29,17 @@ class AvailabilitiesExtension < Spree::Extension
       # def add_availability_tab
       #   # Seems broken, not sure what happened, used to work
       #   add_extension_admin_tab [ :availabilities, { :label => "tab_availability" } ]
-      # end
-      
+      # end      
     end
 
-    # make your helper avaliable in all views
+    Admin::ConfigurationsController.class_eval do
+      before_filter :add_availability_link, :only => :index
+
+      def add_availability_link
+        @extension_links << {:link => admin_availabilities_path, :link_text => t('tab_availability'), :description => t('tab_availability_desc')}
+      end
+    end
+    
     Spree::BaseController.class_eval do
       helper AvailabilitiesHelper
     end
